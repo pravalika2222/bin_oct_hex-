e0e3c60976030e3d1bf03edb9b8ebe04977e4068
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-04 20:45:31 +0530

    Add files via upload

---
+++ bin_oct_hex.py
@@ -0,0 +1,41 @@
+'''binary type'''
+'binary means bi-2'
+# print(bin(7))     #  0b111    number conveting in to binary number
+# print(0b111)      #    7      binary converting in to number
+# print(0B111)      #    7
+
+# print(bin(15))      #  0b111
+# print(0b1111)       #    15
+# print(0B1111)       #    15
+
+# print(bin(26))        #  0b11010
+# print(0b11010)        #     26
+# print(0B11010)        #     26
+
+'''oct type'''
+'oct means oct-8'
+# print(oct(80))         #  0o120
+# print(0o120)           #   80
+# print(0O120)           #   80
+
+# print(oct(25))           #  0o31
+# print(0o31)              #   25
+# print(0O31)              #   25
+
+# print(oct(64))            #  0o100
+# print(0o100)              #    64
+# print(0O100)              #    64
+
+'''hex type'''
+'hex means hex-16'
+# print(hex(72))               #  0x48
+# print(0x48)                  #   72
+# print(0X48)                  #   72
+
+# print(hex(32))              #  0x20
+# print(0x20)                 #   32
+# print(0X20)                 #   32
+
+# print(hex(22))               # 0x16
+# print(0x16)                  #  22
+# print(0X16)                  #  22
